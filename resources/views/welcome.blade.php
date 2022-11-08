8111eccc20df285be5883410c58386b48bf96391
Yobirson <1341423@senati.pe>
2022-11-07 18:34:10 -0600

    senguda actualisacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,25 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Turismo en Peru</title>
+        <body style="background-color:#000000;">
+</body>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     </head>
     <body>
+        <header>
+            <a href="#" class="logo">
+                <span>Turismo en</span> Peru</a>
+                <nav class="navbar">
+                    <a href="#inicio">inicio</a>
+                    <a href="#detalles">detalles</a>
+                    <a href="#acerca de ">acerca de</a>
+                    <a href="#opiniones">opiniones</a>
+                    <a href="#Precios">Precios</a>
+                    <a href="#contactanos">contactanos</a>
+                </nav>
 
+            </header>
 
     </body>
 </html>
