b542306d0fa9152defd380b647dbdc0b0679b6f6
Yobirson <115845685+Yobirson@users.noreply.github.com>
2022-11-15 08:10:42 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Turismo en Peru</title>
-        <body style="background-color:#000000;">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turismo peru</title>
+    <body style="background-color:#000000;">
 </body>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    </head>
-    <body>
-        <header>
-            <a href="#" class="logo">
-                <span>Turismo en</span> Peru</a>
-                <nav class="navbar">
-                    <a href="#inicio">inicio</a>
-                    <a href="#detalles">detalles</a>
-                    <a href="#acerca de ">acerca de</a>
-                    <a href="#opiniones">opiniones</a>
-                    <a href="#Precios">Precios</a>
-                    <a href="#contactanos">contactanos</a>
-                </nav>
 
-            </header>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    </body>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+           
+            </div>
+           <h1>prueva 001</h1>
+           <h1>prueva 002</h1>
+           <h1>prueva 003</h1>
+        </div>
+    
+</div>
+</body>
 </html>
